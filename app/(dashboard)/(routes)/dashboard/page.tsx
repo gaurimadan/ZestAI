d4c7e8ad1f0c50f4b083a2db7b62b056453a7019
gaurimadan <gaurimadan12@gmail.com>
2024-06-24 14:00:57 +0530

    structure defined

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function DashboardPage() {
+    return (
+     <p className="text-6xl text-red-500">hello gauri</p>
+    );
+  }
+  
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+const LandingPage=() => {
+    return(
+        <div className="text-3xl text-green-500">
+            LANDING PAGE 
+        </div>
+    );
+    
+    
+}
+export default LandingPage;
