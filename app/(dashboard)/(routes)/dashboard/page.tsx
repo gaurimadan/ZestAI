4a684b983d2e4086cb1aec7cf261a572d81779ff
gaurimadan <gaurimadan12@gmail.com>
2024-06-24 15:42:17 +0530

    sidebar added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function DashboardPage() {
     return (
+      <div>
      <p className="text-6xl text-red-500">hello gauri</p>
+     <UserButton afterSignOutUrl="/"/>
+     </div>
+
+
     );
   }
   
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
+
 const LandingPage=() => {
     return(
         <div className="text-3xl text-green-500">
             LANDING PAGE 
+       
+        <div>
+        <Link href="/sign-in">
+            <Button>
+                Login
+            </Button>
+        </Link>
+        <Link href="/sign-in">
+            <Button>
+                Register
+            </Button>
+        </Link>
+        </div>
         </div>
     );
     
