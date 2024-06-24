b91c5794ade8fcbe539825028732e8e81a37b8ba
gaurimadan <gaurimadan12@gmail.com>
2024-06-24 19:00:45 +0530

    sidebar updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,12 @@
 import Image from "next/image";
 
 import  Link from "next/link";
+import {Montserrat} from "next/font/google"
+import { cn } from "@/lib/utils";
+
+const montserrat =Montserrat({ weight: "600",subsets:["latin"]});
+
+
 
 const Sidebar =()=>{
     return(
@@ -16,7 +22,7 @@ const Sidebar =()=>{
                 />
 
             </div>
-            <h1 className="text-2xl font-bold">
+            <h1 className={cn("text-2xl font-bold",montserrat.className)}>
                 ZestAI
                 </h1>
             
