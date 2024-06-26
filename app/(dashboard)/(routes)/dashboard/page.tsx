3fb119f20cb79f630987e6bd883ac074178ddc54
gaurimadan <gaurimadan12@gmail.com>
2024-06-26 15:04:34 +0530

    toggle button added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,7 +2,7 @@
 export default function DashboardPage() {
     return (
       <div>
-     <p className="text-6xl text-red-500">hello gauri</p>
+     <p className="text-6xl text-red-500">Welcome</p>
      
      </div>
 
