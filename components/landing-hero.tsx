66c8c7296bfbc373ff46e410ce6911f02da4312d
gaurimadan <gaurimadan12@gmail.com>
2024-07-13 02:48:23 +0530

    landing page updated

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import { useAuth } from "@clerk/nextjs"
+import Link from "next/link";
 import TypewriterComponent from "typewriter-effect"
+import { Button } from "./ui/button";
 
 export const LandingHero =()=>{
     const {isSignedIn}=useAuth();
@@ -24,6 +26,15 @@ export const LandingHero =()=>{
                         />
                 </div>
             </div>
-        </div>
+            <div className="text-sm md:text-xl font-light text-zinc-400">
+                Create curated content with AI.
+            </div>
+        
+        <Link href={isSignedIn?"/dashboard":"/sign-up"}>
+            <Button className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
+                Start Generating for free
+            </Button>
+        </Link>
+           </div>
     )
 }
